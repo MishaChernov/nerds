1c03b737e27155bf82fd62359f0046caf800f4ed
MishaChernov <80amulet63@gmail.com>
2017-03-20 22:34:55 +0200

    Минимизация и поправки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-var link = document.querySelector('.contacts .btn');
-var close = document.querySelector('.btn-close');
 var popup = document.querySelector('.popup');
+var link = document.querySelector('.btn-contacts');
+var close = popup.querySelector('.btn-close');
+var KEY_ESC = 27;
 
 link.addEventListener('click', function(event) {
   event.preventDefault();
@@ -12,8 +13,8 @@ close.addEventListener('click', function(event) {
   popup.classList.remove('popup-show');
 });
 
-window.addEventListener('keydown', function(event){
-  if(event.keyCode === 27) {
+window.addEventListener('keydown', function(event) {
+  if(event.keyCode === KEY_ESC) {
     if(popup.classList.contains('popup-show')){
       popup.classList.remove('popup-show');
     }
